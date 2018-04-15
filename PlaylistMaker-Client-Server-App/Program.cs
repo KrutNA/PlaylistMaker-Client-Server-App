15a1d05f28aea60c6f7f5d9d5fc564f08fe1bda5
KrutNA <31564815+KrutNA@users.noreply.github.com>
2018-04-15 17:21:54 +0300

    Added checking command line  arguments

--- PlaylistMaker-Client-Server-App/Program.cs
+++ PlaylistMaker-Client-Server-App/Program.cs
@@ -4,12 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PlaylistMaker_Client_Server_App
+namespace PlaylistMaker.Client
 {
     class Program
     {
         static void Main(string[] args)
         {
+            if (args.Length == 2)
+            {
+
+            }
+            else
+            {
+                Console.WriteLine(
+                    "Program need only 2 arguments:\n\t1) IP addres of Server\n\t2) Port for connection\nPress any key to exit ");
+                Console.ReadKey();
+            }
         }
     }
 }
---
+++ PlaylistMaker.Server/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;  
+using System.Text;
+using System.Threading;
+
+namespace PlaylistMaker.Server
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
