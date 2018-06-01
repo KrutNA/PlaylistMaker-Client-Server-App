732d566892f3676e0a5da65be8e55d5226292f13
KrutNA <31564815+KrutNA@users.noreply.github.com>
2018-06-01 16:23:58 +0300

    Rebuilding

--- PlaylistMaker.Logic/Properties/AssemblyInfo.cs
+++ PlaylistMaker.Logic/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PlaylistMaker.Logic")]
+[assembly: AssemblyTitle("PlaylistMaker.Logicc")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PlaylistMaker.Logic")]
+[assembly: AssemblyProduct("PlaylistMaker.Logicc")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("33584517-e112-473b-a4b6-89843369fd3c")]
+[assembly: Guid("0707788f-9ed6-45ff-ae24-4078b0196ba2")]
 
 // Version information for an assembly consists of the following four values:
 //
